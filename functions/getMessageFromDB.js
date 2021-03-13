d981cb7447d77eb549799dee2f27f11bbfc57558
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-03-13 00:02:28 -0700

    Update getMessageFromDB.js

--- functions/getMessageFromDB.js
+++ functions/getMessageFromDB.js
@@ -1,5 +1,5 @@
 const Database = require('better-sqlite3');
-const db = new Database('../sql/');const db = new Database('../sql/starBit.db');
+const db = new Database('star.db');
 
 function getMessageFromDatabase(msgid) {
     return (db.prepare('SELECT * FROM starids WHERE msgid = ?').get(msgid) || {}).starid;
