88d2f230152b9e9039115422945e487b9fc290ba
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-03-12 20:52:40 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { Client, Collection } = require ('discord.js');
-const config = require ('../config');
+const config = require ('./config');
 
 const client = new Client({
     disableMentions: 'everyone',
